4b0a5c23e0f4e70cbbc55ec0b14405d823635b0b
MarcosNBJ <marcosnery.comp@gmail.com>
2019-05-17 01:52:34 -0300

    Testing JSON request returned values identification
    Signed-off-by: MarcosNBJ <marcosnery.comp@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file, request
+from anastruct import SystemElements
+from matplotlib import pyplot as plt
+import io
+import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route('/test', methods=['GET'])
+def hellodois():
+    return jsonify({"tipo":0,
+                    "apoio1":1,
+                    "apoio2":2,
+                    "apoio1p":1,
+                    "apoio2p":3,
+                    "cargap":2,
+                    "cargam":-10})
+
+
+
+@app.route('/get_diagram')
 def get_diagram():
 
-    return jsonify({"Teste":teste})
+    r = requests.get('http://0.0.0.0:5000/test')
+    
+    return jsonify(r.json())
+    
 
 
 if __name__ == '__main__':
